f40163e176a92b2a36b7b4f6e4cbaf10bc7d75ea
super-era <subha.anne.perera@gmail.com>
2021-12-28 20:32:18 +1100

    Adding calendar task-tracker functionality - day selection

--- todo_app/lib/page/tracker_page.dart
+++ todo_app/lib/page/tracker_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-class TrackerPage extends StatelessWidget {
+class TrackerPage extends StatefulWidget {
   @override
+  _TrackerPageState createState() => _TrackerPageState();
+}
+
+class _TrackerPageState extends State<TrackerPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  
   Widget build(BuildContext content) => Scaffold(
     appBar: AppBar(
       title: const Text('Task Tracker'),
     ),
     body: TableCalendar(
+      calendarFormat: _calendarFormat,
+      onFormatChanged: (format) {
+        setState(() {
+          _calendarFormat = format;
+        });
+      },
       firstDay: DateTime.utc(2010, 1, 1),
       lastDay: DateTime.utc(2030, 12, 31),
       focusedDay: DateTime.now(),
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay;
+        });
+      },
+      onPageChanged: (focusedDay) {
+        _focusedDay = focusedDay;
+      },
     ),
   );
 }
