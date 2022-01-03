c9f24189bbaacdbeae2c2f818c8c47ac10b71a73
super-era <subha.anne.perera@gmail.com>
2022-01-03 22:23:37 +1100

    Adding calendar task-tracker functionality - events

--- todo_app/lib/page/tracker_page.dart
+++ todo_app/lib/page/tracker_page.dart
@@ -9,6 +9,85 @@ class TrackerPage extends StatefulWidget {
 }
 
 class _TrackerPageState extends State<TrackerPage> {
+  final Map<DateTime, List<CleanCalendarEvent>> _events ={
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day): [
+      CleanCalendarEvent('Event A',
+        startTime: DateTime(DateTime.now().year, DateTime.now().month,
+            DateTime.now().day, 10, 0),
+        endTime: DateTime(DateTime.now().year, DateTime.now().month,
+            DateTime.now().day, 12, 0),
+        description: 'A special event',
+        color: Colors.blue.shade700,
+      ),
+    ],
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 2): [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+    ],
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 3): [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+      CleanCalendarEvent('Event D',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.amber),
+      CleanCalendarEvent('Event E',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.deepOrange),
+      CleanCalendarEvent('Event F',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.green),
+      CleanCalendarEvent('Event G',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.indigo),
+      CleanCalendarEvent('Event H',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.brown),
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _handleNewDate(DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day)
+    );
+  }
+
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
@@ -31,40 +110,11 @@ class _TrackerPageState extends State<TrackerPage> {
             locale: 'en_EN',
           ),
         ),
-        _buildEventList()
       ],
     ),
   ); 
 
-  Widget _buildEventList() {
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.all(0.0),
-        itemBuilder: (BuildContext context, int index) {
-          final CleanCalendarEvent event = _selectedEvents[index];
-          final String start =
-            DateFormat('HH:mm').format(event.startTime).toString();
-          final String end =
-            DateFormat('HH:mm').format(event.endTime).toString();
-          return ListTile(
-            contentPadding:
-              EdgeInsets.only(left: 2.0, right: 8.0, top: 2.0, bottom: 2.0),
-            leading: Container(
-              width: 10.0,
-              color: event.color,
-            ),
-            title: Text(event.summary),
-            subtitle:
-              event.description.isNotEmpty ? Text(event.description) : null,
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text(start), Text(end)],
-            ),
-            onTap: () {},
-          );
-        },
-        itemCount: _selectedEvents.length,
-      ),
-    );
+  void _handleNewDate(date) {
+    print('Date selected: $date');
   }
 }
